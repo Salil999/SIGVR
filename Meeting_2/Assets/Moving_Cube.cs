1b642e0b7099d68124c0ab8f2f49aa3bfe80cb92
Salil999 <ssaxen4@illinois.edu>
2018-02-06 14:06:58 -0600

    Meeting 2 project files added in

---
+++ Meeting_2/Assets/Moving_Cube.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moving_Cube : MonoBehaviour {
+
+    public GameObject cube;
+    public GameObject camera;
+
+    public float maxSpeed;
+    public float acceleration;
+    public float speed;
+
+    // Use this for initialization
+    void Start () {
+
+		// Get the current game object
+        cube = this.gameObject;
+
+        maxSpeed = 1.0f;
+        acceleration = 0.1f;
+        speed = 0.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        // Create the new vector that this cube is going to be at
+//		float currentZPosition = cube.transform.position.z;
+//        Vector3 newPosition = new Vector3 (
+//            cube.transform.position.x,
+//            cube.transform.position.y,
+//			currentZPosition + (float)0.01
+//        );
+//        cube.transform.position = newPosition;
+
+
+//      // This doesn't work - why?
+//      cube.transform.position.z = cube.transform.position.z + (float)0.01;
+//      cube.transform.position.z += (float)0.01;
+
+
+//      // Sets the speed with acceleration
+//      speed = speed + (acceleration * Time.deltaTime);
+//      Vector3 newPosition = new Vector3 (
+//          cube.transform.position.x,
+//          cube.transform.position.y,
+//          cube.transform.position.z + (speed * Time.deltaTime)
+//      );
+//      cube.transform.position = newPosition;
+
+
+//      // Sets the speed with acceleration, but has a limit on the max speed
+//      if (speed < maxSpeed) {
+//          speed = speed + (acceleration * Time.deltaTime);
+//      }
+//      Vector3 newPosition = new Vector3 (
+//          cube.transform.position.x,
+//          cube.transform.position.y,
+//          cube.transform.position.z + (speed * Time.deltaTime)
+//      );
+//      cube.transform.position = newPosition;
+
+    }
+}
---
+++ Meeting_2/Assets/Rotate_Sphere.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate_Sphere : MonoBehaviour {
+
+    public GameObject sphere;
+    public float speedScale;
+
+    // Use this for initialization
+    void Start () {
+        // Get the current game object
+        sphere = this.gameObject;
+
+        speedScale = 50.0f;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        sphere.transform.Rotate (Vector3.up * Time.deltaTime * speedScale);
+    }
+}
